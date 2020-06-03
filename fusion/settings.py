1974c550793b47a0488176149e13d85fab4106cc
Cauã Bernardino <cbernardino@live.com>
2020-06-03 16:42:42 -0300

    DEBUG FALSE

--- fusion/settings.py
+++ fusion/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0v40_ozw=01r%7a__^3_*trb#ksvc!yawzshuax6%(ncwi9q+3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
